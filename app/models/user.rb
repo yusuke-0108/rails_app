c900aa5c39b804a4ae433d0120c63fb2d7808452
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-09-23 15:10:47 +0900

    user model validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 20}
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,30 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 21
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 245 + "@examle.com"
+    assert_not @user.valid?
+  end
+
+  test "email shouls be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
