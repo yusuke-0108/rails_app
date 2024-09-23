7aea021f13a305a4745d5911023a9d5eef621f3c
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-09-23 16:16:26 +0900

    secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 20}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: true
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_23_060005) do
+ActiveRecord::Schema.define(version: 2024_09_23_061727) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
