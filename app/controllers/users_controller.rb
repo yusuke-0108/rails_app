676eee7cf4852ab234d8ce47e12728969845f94f
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-09-25 12:40:15 +0900

    Create User registration form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user.id)
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
