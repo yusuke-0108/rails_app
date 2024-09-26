9e95a856c0ebb909b7e1db8ad3c710c3ae876cfa
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-09-26 11:53:17 +0900

    Use Bootstrap

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user_path(user)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+end
